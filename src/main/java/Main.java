20c03c235ec1a8e341b3cec2893041abe29ab69b
1GodLess5 <gacekalbert@seznam.cz>
2024-01-21 13:43:10 +0100

    new class Main -> created basic flow of the program

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    private final static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        do {
+            System.out.println("Write number, then type of operation (+, -, *, /) and then write number again.");
+            double leftNumber;
+            double rightNumber;
+            char operation;
+
+            while (true){
+                try {
+                    System.out.print("First number: ");
+                    leftNumber = scanner.nextDouble();
+                    System.out.print("Operation: ");
+                    operation = scanner.next().charAt(0);
+                    if (operation != '+' && operation != '-' && operation != '*' && operation != '/'){
+                        System.out.println("Invalid input.");
+                        continue;
+                    }
+                    System.out.print("Second number: ");
+                    rightNumber = scanner.nextDouble();
+
+                    break;
+                } catch (InputMismatchException e){
+                    System.out.println("Invalid input.");
+                    scanner.nextLine();
+                }
+            }
+
+
+
+            System.out.println("Left: " + leftNumber + ", operation: " + operation + ", right: " + rightNumber);
+        } while (true);
+    }
+}
