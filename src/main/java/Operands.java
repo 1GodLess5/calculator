c0465b81635fd4a9a60d5d3097394e0d3db442e1
1GodLess5 <gacekalbert@seznam.cz>
2024-01-21 13:46:36 +0100

    new class Operands

---
+++ src/main/java/Operands.java
@@ -0,0 +1,17 @@
+public class Operands {
+    private final double leftNumber;
+    private final double rightNumber;
+
+    public Operands(double leftNumber, double rightNumber) {
+        this.leftNumber = leftNumber;
+        this.rightNumber = rightNumber;
+    }
+
+    public double getLeftNumber() {
+        return leftNumber;
+    }
+
+    public double getRightNumber() {
+        return rightNumber;
+    }
+}
